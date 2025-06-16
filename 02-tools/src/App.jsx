e6ec4519475f7b7f9be27b2fc74c56d779913b1a
Frank Pigeon Jr <fpigeon@flexion.us>
2025-06-16 15:37:54 -0500

    feat: starts Order component

--- 02-tools/src/App.jsx
+++ 02-tools/src/App.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => (
   <div>
     <h1>Padre Gino's - Order Now</h1>
-    <Pizza
-      name="Pepperoni Pizza"
-      description="A classic pizza with pepperoni slices and cheese."
-      image="/public/pizzas/pepperoni.webp"
-    />
-    <Pizza
-      name="Hawaiian Pizza"
-      description="A controversial pizza with ham, pineapple, and cheese."
-      image="/public/pizzas/hawaiian.webp"
-    />
-    <Pizza
-      name="Meat Lovers Pizza"
-      description="A hearty pizza"
-      image="/public/pizzas/big_meat.webp"
-    />
+    <Order />
   </div>
 );
 
