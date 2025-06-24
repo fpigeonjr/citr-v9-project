ad7d551aac2cf63ebe76de0c81587e436b966c0a
Frank Pigeon Jr <fpigeon@flexion.us>
2025-06-24 08:34:05 -0500

    fix: update staleTime format for better readability

--- 02-tools/src/routes/past.lazy.jsx
+++ 02-tools/src/routes/past.lazy.jsx
@@ -12,7 +12,7 @@ function PastOrdersRoute() {
   const { isLoading, data } = useQuery({
     queryKey: ["past-orders", page],
     queryFn: () => getPastOrders(page),
-    staleTime: 30000,
+    staleTime: 30_000, // 30 seconds
   });
 
   if (isLoading) {
